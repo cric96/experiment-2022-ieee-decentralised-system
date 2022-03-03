c4648151ddb0515bedaddf406dc3a7e9072766d4
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-03 16:12:59 +0100

    style: improve code readability

--- src/main/scala/it/unibo/scafi/incarnation/BlockSWithProcesses.scala
+++ src/main/scala/it/unibo/scafi/incarnation/BlockSWithProcesses.scala
@@ -10,7 +10,7 @@ trait BlockSWithProcesses {
   type Distance =
     Boolean => Double // or in general, it could be type Distance[D: Numeric] = Boolean => D, or another type context
 
-  case class LeaderProcessData(symmetryBreaker: SymmetryBreaker, distanceFromLeader: Double)
+  case class LeaderProcessOutput(symmetryBreaker: SymmetryBreaker, distanceFromLeader: Double)
   case class LeaderProcessInput(
       localLeader: ID,
       localId: ID,
@@ -27,15 +27,15 @@ trait BlockSWithProcesses {
       metric: Metric,
       distance: Distance
   ): ID = {
-    val default = id -> LeaderProcessData(symmetryBreaker, 0.0)
+    val default = id -> LeaderProcessOutput(symmetryBreaker, 0.0)
     rep(default) { case (leadId, leadData) =>
-      val leaders = sspawn2[ID, LeaderProcessInput, LeaderProcessData](
+      val leaders = sspawn2[ID, LeaderProcessInput, LeaderProcessOutput](
         processDefinition,
         Set(leadId),
         LeaderProcessInput(leadId, id, leadData.symmetryBreaker, radius, metric, distance)
       )
       node.put("howManyProcess", leaders.size)
-      val closeEnough = leaders.filter { case (_, LeaderProcessData(_, distance)) => distance < radius }
+      val closeEnough = leaders.filter { case (_, LeaderProcessOutput(_, distance)) => distance < radius }
       val best = selectLeader(closeEnough).getOrElse(default)
       best
     }._1
@@ -48,30 +48,30 @@ trait BlockSWithProcesses {
       metric: Metric,
       distance: Distance
   ): ID = {
-    val default = id -> LeaderProcessData(symmetryBreaker, 0.0)
+    val default = id -> LeaderProcessOutput(symmetryBreaker, 0.0)
     share(default) { case ((leadId, leadData), nbrField) =>
       val sources = includingSelf.unionHood(nbrField())
       node.put("sources", sources.map(_._1))
       val processId: Set[ID] = sources.map { case (id, _) => id }
-      val leaders = sspawn2[ID, LeaderProcessInput, LeaderProcessData](
+      val leaders = sspawn2[ID, LeaderProcessInput, LeaderProcessOutput](
         processDefinition,
         processId,
         LeaderProcessInput(leadId, id, leadData.symmetryBreaker, radius, metric, distance)
       )
       node.put("howManyProcess", leaders.size)
-      val closeEnough = leaders.filter { case (_, LeaderProcessData(_, distance)) => distance < radius }
-      val best = selectLeader(closeEnough).getOrElse(default)
+      // val closeEnough = leaders.filter { case (_, LeaderProcessOutput(_, distance)) => distance < radius }
+      val best = selectLeader(leaders).getOrElse(default)
       best
     }._1
   }
 
-  private val processDefinition: ID => LeaderProcessInput => POut[LeaderProcessData] = id =>
-    breaker => {
-      val status = insideBubble(id)(breaker)
+  private val processDefinition: ID => LeaderProcessInput => POut[LeaderProcessOutput] = id =>
+    input => {
+      val status = insideBubble(id)(input)
       mux(status == Terminated || status == External) {
-        POut(LeaderProcessData(0, Double.PositiveInfinity), status)
+        POut(LeaderProcessOutput(Double.NegativeInfinity, Double.PositiveInfinity), status)
       } {
-        POut(expandBubble(id)(breaker), Output)
+        POut(expandBubble(id)(input), Output)
       }
     }
 
@@ -85,16 +85,16 @@ trait BlockSWithProcesses {
       }
     }
 
-  private val expandBubble: ID => LeaderProcessInput => LeaderProcessData =
+  private val expandBubble: ID => LeaderProcessInput => LeaderProcessOutput =
     processId => { case LeaderProcessInput(localLeader, localId, symmetryBreaker, _, _, distance) =>
       val source = processId == localId && localLeader == localId
       val gradient = distance(source)
-      LeaderProcessData(broadcastAlong(source, gradient, symmetryBreaker), gradient)
+      LeaderProcessOutput(broadcastAlong(source, gradient, symmetryBreaker), gradient)
     }
 
-  private def selectLeader(leaders: Map[ID, LeaderProcessData]): Option[(ID, LeaderProcessData)] = {
-    leaders.reduceOption[(ID, LeaderProcessData)] {
-      case (leaderA @ (idA, LeaderProcessData(breakerA, _)), leaderB @ (idB, LeaderProcessData(breakerB, _))) =>
+  private def selectLeader(leaders: Map[ID, LeaderProcessOutput]): Option[(ID, LeaderProcessOutput)] = {
+    leaders.reduceOption[(ID, LeaderProcessOutput)] {
+      case (leaderA @ (idA, LeaderProcessOutput(breakerA, _)), leaderB @ (idB, LeaderProcessOutput(breakerB, _))) =>
         if ((breakerA, idA) > (breakerB, idB)) {
           leaderA
         } else {
