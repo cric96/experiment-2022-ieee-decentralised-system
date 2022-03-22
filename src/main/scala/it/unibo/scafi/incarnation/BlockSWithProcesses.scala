b1de081871ef21b699cad851e3641a1e7c0e2531
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-22 18:16:08 +0100

    avoid edge nodes

--- src/main/scala/it/unibo/scafi/incarnation/BlockSWithProcesses.scala
+++ src/main/scala/it/unibo/scafi/incarnation/BlockSWithProcesses.scala
@@ -31,7 +31,7 @@ trait BlockSWithProcesses {
     *   simmetry breaker used during the leader election
     * @param radius
     *   the maxiumum influence area of a leader
-    * @param distance
+    * @param distanceFunction
     *   the strategy used to compute the distance from the leader
     */
   case class LeaderProcessInput[S](
@@ -39,7 +39,8 @@ trait BlockSWithProcesses {
       localId: ID,
       breaker: S,
       radius: Double,
-      distance: Distance
+      distanceFunction: Distance,
+      distanceFromLeader: Double
   )
 
   /** This function produces a field of id resulted from a distributed leader election process.
@@ -61,18 +62,18 @@ trait BlockSWithProcesses {
   ): ID = {
     val default = id -> LeaderProcessOutput[S](symmetryBreaker, 0.0)
     rep(default) { case (leadId, leadOutput) =>
-      val shouldStartProcess = id == leadId || (symmetryBreaker, id) > (leadOutput.symmetryBreaker, leadId)
+      val candidate = id == leadId
       // compute the leaders using processes, in jointing point multiple leader could exists
       val leaders: Map[ID, LeaderProcessOutput[S]] = sspawn2[ID, LeaderProcessInput[S], LeaderProcessOutput[S]](
         processDefinition,
-        mux(shouldStartProcess)(Set(id))(Set.empty), // a process is spawn only if I am the local candidate
-        LeaderProcessInput(leadId, id, symmetryBreaker, radius, distanceFunction)
+        mux(candidate)(Set(id))(Set.empty), // a process is spawn only if I am the local candidate
+        LeaderProcessInput(leadId, id, symmetryBreaker, radius, distanceFunction, leadOutput.distanceFromLeader)
       )
       node.put("leaders", leaders)
       val closeEnough = leaders.filter { case (_, LeaderProcessOutput(_, distance)) => distance < radius }
       node.put("close-enough", closeEnough)
       // choose the leader using the breaking symmetry value
-      selectLeader(closeEnough).getOrElse(default)
+      selectLeader(leaders + default).getOrElse(default)
     }._1
   }
 
@@ -90,21 +91,39 @@ trait BlockSWithProcesses {
     }
 
   private def insideBubble[S]: ID => LeaderProcessInput[S] => (Status, Double) =
-    processId => { case LeaderProcessInput(localLeader, uid, _, radius, distanceFunction) =>
+    processId => { case LeaderProcessInput(localLeader, uid, _, radius, distanceFunction, localDistanceLeader) =>
+      val distanceFromLeader = distanceFunction(processId == uid) // distance from the leader
       optBranch(processId == uid && uid != localLeader) {
         // started the process, but I am not the leader anymore, so I suppress that process
         (Terminated, Double.PositiveInfinity)
       } {
-        val distanceFromLeader = distanceFunction(processId == uid) // distance from the leader
+
         val inBubble = distanceFromLeader <= radius // check if this zone is inside the bubble
+        // check if any node near to me have this zone activate
         val anyNodeInBubble = includingSelf.anyHood(nbr(localLeader) == processId)
-        (mux(inBubble && anyNodeInBubble)(Output)(External), distanceFromLeader)
+
+        /** edge condition: happens when a node is between two zones:
+          *
+          * a -- x -- a
+          *
+          * x should be external to the a bubble.
+          */
+        val minLeader = excludingSelf.minHoodSelector(nbr(distanceFromLeader))(nbr(localLeader))
+        val maxLeader = excludingSelf.maxHoodSelector(nbr(distanceFromLeader))(nbr(localLeader))
+        val neighboursCount = excludingSelf.sumHood(1)
+        val edgeCondition =
+          minLeader == maxLeader && neighboursCount > 1 && localLeader != processId && localDistanceLeader != 0.0
+        val status =
+          mux(inBubble && anyNodeInBubble) {
+            mux(edgeCondition)(External)(Output)
+          }(External)
+        (status, distanceFromLeader)
       }
     }
 
   private def expandBubble[S: Bounded]: Double => ID => LeaderProcessInput[S] => LeaderProcessOutput[S] =
     gradient =>
-      processId => { case LeaderProcessInput(_, uid, breaker, _, _) =>
+      processId => { case LeaderProcessInput(_, uid, breaker, _, _, _) =>
         val source = processId == uid
         // broadcast the breaker of this leader in the entire zone where
         LeaderProcessOutput(broadcastAlong(source, gradient, breaker), gradient)
